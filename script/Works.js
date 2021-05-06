c0c30a18e34c69949c69254373be2b2f9ccb5d66
MinBeomgi <alsqjarlwkd@naver.com>
2021-05-06 20:26:00 +0900

    모달창 뜰때 container 클래스 사라지게 변경 & Home.js추가
    모달창 뜰때 container 클래스 사라지게 변경 & Home.js추가

--- script/Works.js
+++ script/Works.js
@@ -1,11 +1,14 @@
 let MenuBtn = document.querySelector(`.Nav_Right`);
 let Close = document.querySelector(`.modal_close`);
+let MainContainer = document.querySelector('.container');
 function MenuBtnClick(){
     console.log(`클릭`);
+    MainContainer.style.display='none';
     document.querySelector('.modal_wrap').style.display ='block';
     document.querySelector('.black_bg').style.display ='block';
 }
 function offClick() {
+    MainContainer.style.display='block'
     document.querySelector('.modal_wrap').style.display ='none';
     document.querySelector('.black_bg').style.display ='none';
 }
